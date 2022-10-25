8307dfa58339b04b835311a4df067a7440827536
Dave Hyman <55147796+davemhyman@users.noreply.github.com>
2022-10-25 14:57:00 -0700

    Update input.py

--- input.py
+++ input.py
@@ -23,20 +23,20 @@ sim.set_source( # set vent/fissure info
 #
 #-------------------------------------------------------------------------------
 sim.set_vent_props( # set lava properties @ vents
-    temperature_vent_C  = 1100, # deg C
-    viscosity_vent      = 100, # Pa s
-    cryst_vent          = 0.00  # erupted crystal fraction
+    temperature_vent_C  = 0.0, # deg C
+    viscosity_vent      = 0.0, # Pa s
+    cryst_vent          = 0.0  # erupted crystal fraction
     )
 
 #
 #-------------------------------------------------------------------------------
 sim.set_lava_props( # set lava properties throughout
-    liquid_density      = 2700,   # kg m-3
-    porosity            = 0.4,
-    lava_specific_heat  = 1500,   # J kg-1 K-1
-    lava_diffusivity    = 5e-7, # m2 s-1
+    liquid_density      = 0.0,   # kg m-3
+    porosity            = 0.0,
+    lava_specific_heat  = 0.0,   # J kg-1 K-1
+    lava_diffusivity    = 0.0, # m2 s-1
     lava_conductivity   = None,    # W m-1 K-1
-    lava_emissivity     = 0.95
+    lava_emissivity     = 0.0
     )
 
 #
@@ -49,29 +49,29 @@ sim.set_rheo( # set rheological properties
 #
 #-------------------------------------------------------------------------------
 sim.set_ambient( # set ambient properties
-    atm_temperature     = 300, # K
-    h_conv              = 50,   # m s-1
-    rainfall            = 0,   # m s-1
-    ground_temperature  = 300  # K
+    atm_temperature     = 0.0, # K
+    h_conv              = 0.0,   # m s-1
+    rainfall            = 0.0,   # m s-1
+    ground_temperature  = 0.0  # K
     )
 
 #
 #-------------------------------------------------------------------------------
 sim.set_numerics( # set numerical method details
     method = 'OHA',
-    efficiency_min      = 10,   # minmum allowable model-clock ratio
-    efficiency_max      = 10000, # maximum allowable model-clock ratio
-    cfl_max             = 0.5,
-    dt_max              = 10.,
-    fraction_to_freeze  = 1.0,  # fraction of freezable lava
-    tiny_flow           = 0.1,  # min thickness of lava (m)
+    efficiency_min      = 0.0,   # minmum allowable model-clock ratio
+    efficiency_max      = 0.0, # maximum allowable model-clock ratio
+    cfl_max             = 0.0,
+    dt_max              = 0.0,
+    fraction_to_freeze  = 0.0,  # fraction of freezable lava
+    tiny_flow           = 0.0,  # min thickness of lava (m)
     )
 
 #
 #-------------------------------------------------------------------------------
 sim.set_runtime(
     max_iter = None, #one of them can be none, so the default value for both is none, if both none run till killed
-    max_time =  12*3600 # s
+    max_time =  0.0 # s
     )
 #
 #-------------------------------------------------------------------------------
